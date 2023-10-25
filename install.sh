95bd91b0524945727cb9c93bcb97a37aaa01847e
elPytel <jaroslav.korner1@gmail.com>
2023-10-25 11:11:47 +0200

    Install script created

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#By Pytel
+
+# check if pip is installed
+if ! [ -x "$(command -v pip)" ]; then
+  echo 'Error: pip is not installed.' >&2
+  exit 1
+fi
+# install required libraries
+echo "Installing required libraries..."
+pip install -r requirements.txt
+
+
+# check if systemd is installed
+if ! [ -x "$(command -v systemctl)" ]; then
+  echo 'Error: systemctl is not installed.' >&2
+  exit 1
+fi
+
+# change path to script
+echo "Changing path to script..."
+sed -i "s|<path>|$(pwd)|g" ./discrodBashAccessor.service
+
+# set up system service
+echo "Setting up system service..."
+cp ./discrodBashAccessor.service /etc/systemd/system/discrodBashAccessor.service
+systemctl daemon-reload
+systemctl enable discrodBashAccessor.service
+systemctl start discrodBashAccessor.service
+
+# check if service is running
+if ! systemctl is-active --quiet discrodBashAccessor.service; then
+  echo 'Error: discrodBashAccessor.service is not running.' >&2
+  exit 1
+fi
+
+echo "Installation completed."
